102ab6acada0886690ef1a7629481f194784ad2b
jaspreetsanghra <jaspreet.sanghra@gmail.com>
2021-10-16 17:24:02 -0400

    Start of v2 - docker image with ubunutu

---
+++ entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/sh -l
+
+filepath=$1
+sslusername=$2
+sslpassword=$3
+sslsecretpassword=$4
+sslclientid=$5
+istest=$6
+
+echo "Installing JavaRuntime Environment"
+sudo apt-get install -y default-jre
+
+if [ ! -d "CodeSignTool-v1.2.0" ]; then
+    echo "Installing unzip"
+    sudo apt-get install -y unzip
+
+    echo "Downloading CodeSignTool"
+    curl https://www.ssl.com/download/29764/ --output CodeSignTool.zip
+    echo "Extracting CodeSignTool"
+    unzip -o CodeSignTool.zip
+
+    cd CodeSignTool-v1.2.0
+    mkdir -p "ssl-output"
+else
+    cd CodeSignTool-v1.2.0
+fi
+
+if [ ${istest} = true ] ; then
+  # TESTING ONLY - Sandbox creds
+  echo "Running Test"
+  printf "CLIENT_ID=qOUeZCCzSqgA93acB3LYq6lBNjgZdiOxQc-KayC3UMw\nOAUTH2_ENDPOINT=https://oauth-sandbox.ssl.com/oauth2/token\nCSC_API_ENDPOINT=https://cs-try.ssl.com\nTSA_URL=http://ts.ssl.com" > 'conf/code_sign_tool.properties'
+  bash -eu CodeSignTool.sh sign -username='esigner_demo' -password='esignerDemo#1' -totp_secret='RDXYgV9qju+6/7GnMf1vCbKexXVJmUVr+86Wq/8aIGg=' -input_file_path="${filepath}" -output_dir_path='ssl-output'
+else
+  # SINGLE FILE
+  echo "Running Real UseCase"
+  printf "CLIENT_ID=${{ inputs.sslclientid }}\nOAUTH2_ENDPOINT=https://login.ssl.com/oauth2/token\nCSC_API_ENDPOINT=https://cs.ssl.com\nTSA_URL=http://ts.ssl.com" > 'conf/code_sign_tool.properties'
+  bash -eu CodeSignTool.sh sign -username="${sslusername}" -password="${sslpassword}"-totp_secret="${sslsecretpassword}" -input_file_path="${filepath}" -output_dir_path='ssl-output'
+fi
+
+echo "Copying Signed File to root"
+ls ssl-output/
+sudo cp -r ./ssl-output/* ../
+
